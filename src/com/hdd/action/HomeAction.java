c89d5084964c91832cca66ccb8c91dc1cd023d64
Hoàng Duy <hoangduy@Duys-MacBook-Pro.local>
2018-05-19 10:06:18 +0700

    Init new Project src

---
+++ src/com/hdd/action/HomeAction.java
@@ -0,0 +1,12 @@
+package com.hdd.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport {
+
+	public static final String HOME = "home";
+
+	public String execute() {
+		return HOME;
+	}
+}
