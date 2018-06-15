2d1bce983516d933906397d603f8a2bd0850d693
rocsi16011996 <14520362@gm.uit.edu.vn>
2018-06-15 19:41:23 +0700

    update product sql

--- SQL/Products.sql
+++ SQL/Products.sql
@@ -10,10 +10,11 @@ CREATE TABLE `Products` (
           `ProductType` int,
           `ProductTrend` int,
           `UpdatedAt` datetime(6) NOT NULL, 
+          `Rating` smallint,
           CONSTRAINT `PK_Products` PRIMARY KEY (`Id`)
       );
       
-insert into Products values ("P1", 100, '2018/06/10',"Iphone X",0, "./img/iphone-x.png","Thông số kỹ thuật
+insert into Products values ("P1", 100, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 OLED, 5.8, Super Retina
 •	Hệ điều hành:
@@ -32,8 +33,8 @@ Apple A11 Bionic 6 nhân
 1 Nano SIM, Hỗ trợ 4G
 •	Dung lượng pin:
 2716 mAh, có sạc nhanh
-",290000000,3,1,'2018/06/10');
-insert into Products values ("P2", 200, '2018/06/10',"Iphone X-256",0, "./img/iphone-x-256gb.png","Thông số kỹ thuật
+",0, "/img/iphone-x.png","Iphone X",29000000,3,1,'2018/06/10',3);
+insert into Products values ("P2", 200, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 OLED, 5.8, Super Retina
 •	Hệ điều hành:
@@ -52,8 +53,8 @@ Apple A11 Bionic 6 nhân
 1 Nano SIM, Hỗ trợ 4G
 •	Dung lượng pin:
 2716 mAh, có sạc nhanh
-",340000000,3,1,'2018/06/10');
-insert into Products values ("P3", 300, '2018/06/10',"Samsung S8",0, "./img/samsung-s8.png","Thông số kỹ thuật
+",0, "/img/iphone-x-256gb.png","Iphone X-256",34000000,3,1,'2018/06/10',4);
+insert into Products values ("P3", 300, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 Super AMOLED, 5.8, Quad HD+ (2K+)
 •	Hệ điều hành:
@@ -75,8 +76,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 HOTMua sim Mobi Big 60 (3GB data/ tháng). Giá từ 80.000đ
 •	Dung lượng pin:
 3000 mAh, có sạc nhanh
-",150000000,3,1,'2018/06/10');
-insert into Products values ("P4", 500, '2018/06/10',"Samsung A8",0, "./img/samsung-a8.png","Thông số kỹ thuật
+",0, "/img/samsung-s8.png","Samsung S8",15000000,3,1,'2018/06/10',3);
+insert into Products values ("P4", 500, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 Super AMOLED, 6, Full HD+
 •	Hệ điều hành:
@@ -98,8 +99,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 HOTMua sim Mobi Big 60 (3GB data/ tháng). Giá từ 80.000đ
 •	Dung lượng pin:
 3500 mAh, có sạc nhanh
-",130000000,3,0,'2018/06/10');
-insert into Products values ("P5", 400, '2018/06/10',"Iphoen 6SPLUS",0, "./img/iphone-6s-plus.png","Thông số kỹ thuật
+",0, "/img/samsung-a8.png","Samsung A8",13000000,3,0,'2018/06/10',4);
+insert into Products values ("P5", 400, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 LED-backlit IPS LCD, 5.5, Retina HD
 •	Hệ điều hành:
@@ -119,8 +120,8 @@ Apple A9 2 nhân 64-bit
 HOTMua sim Mobi Big 60 (3GB data/ tháng). Giá từ 80.000đ
 •	Dung lượng pin:
 2750 mAh
-",120000000,3,0,'2018/06/10');
-insert into Products values ("P6", 250, '2018/06/10',"Sony Xperia",0, "./img/sony-xperia-xz1.png","Thông số kỹ thuật
+",0, "/img/iphone-6s-plus.png","Iphone 6SPLUS",12000000,3,0,'2018/06/10',4);
+insert into Products values ("P6", 250, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 IPS HDR LCD, 5.2, Full HD
 •	Hệ điều hành:
@@ -141,8 +142,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 2 Nano SIM, Hỗ trợ 4G
 •	Dung lượng pin:
 2700 mAh, có sạc nhanh
-",110000000,3,0,'2018/06/10');
-insert into Products values ("P7", 350, '2018/06/10',"Huawei P20 Pro",0, "./img/huawei-p20-pro.png","Thông số kỹ thuật
+",0, "/img/sony-xperia-xz1.png","Sony Xperia",11000000,3,0,'2018/06/10',2);
+insert into Products values ("P7", 350, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 IPS LCD, 5.84, Full HD+
 •	Hệ điều hành:
@@ -164,8 +165,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 HOTMua sim Mobi Big 60 (3GB data/ tháng). Giá từ 80.000đ
 •	Dung lượng pin:
 3000 mAh, có sạc nhanh
-",70000000,3,0,'2018/06/10');
-insert into Products values ("P8", 450, '2018/06/10',"Oppo F7",0, "./img/oppo-f7.png","Thông số kỹ thuật
+",0, "/img/huawei-p20-pro.png","Huawei P20 Pro",7000000,3,0,'2018/06/10',3);
+insert into Products values ("P8", 450, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 LTPS LCD, 6.23, Full HD+
 •	Hệ điều hành:
@@ -187,8 +188,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 HOTMua sim Mobi Big 60 (3GB data/ tháng). Giá từ 80.000đ
 •	Dung lượng pin:
 3400 mAh
-",100000000,3,0,'2018/06/10');
-insert into Products values ("P9", 550, '2018/06/10',"Vivo V9",0, "./img/vivo-v9.png","Thông số kỹ thuật
+",0, "/img/oppo-f7.png","Oppo F7",10000000,3,0,'2018/06/10',4);
+insert into Products values ("P9", 550, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 IPS LCD, 6.3, Full HD+
 •	Hệ điều hành:
@@ -209,8 +210,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 2 Nano SIM, Hỗ trợ 4G
 •	Dung lượng pin:
 3260 mAh
-",80000000,3,2,'2018/06/10');
-insert into Products values ("P13", 550, '2018/06/10',"Nokia 7",0, "./img/nokia-7.png","Thông số kỹ thuật
+",0, "/img/vivo-v9.png","Vivo V9",8000000,3,2,'2018/06/10',5);
+insert into Products values ("P13", 550, '2018/06/10',"Thông số kỹ thuật
 Màn hình:	IPS LCD, 6, Full HD+
 Hệ điều hành:	Android 8.0 (Oreo)
 Camera sau:	12 MP và 13 MP (2 camera)
@@ -222,8 +223,8 @@ Thẻ nhớ:	MicroSD, hỗ trợ tối đa 256 GB
 Thẻ SIM:
 2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G
 
-Dung lượng pin:	3800 mAh",90000000,3,1,'2018/06/10');
-insert into Products values ("P10",450, '2018/06/10',"HTC U",0, "./img/htc-u.png","Thông số kỹ thuật
+Dung lượng pin:	3800 mAh",0, "/img/nokia-7.png","Nokia 7",9000000,3,1,'2018/06/10',3);
+insert into Products values ("P10",450, '2018/06/10',"Thông số kỹ thuật
 •	Màn hình:
 LTPS LCD, 6.0, Full HD+
 •	Hệ điều hành:
@@ -245,8 +246,8 @@ MicroSD, hỗ trợ tối đa 256 GB
 HOTMua sim Mobi Big 60 (3GB data/ tháng). Giá từ 80.000đ
 •	Dung lượng pin:
 3410 mAh
-",30000000,3,1,'2018/06/10');
-insert into Products values ("P11", 50, '2018/06/10',"Macbook Air",0, "./img/apple-macbook-air.jpg","- CPU: Intel Core i5, Dual - Core, 1.8GHz
+",0, "/img/htc-u.png","HTC U",3000000,3,1,'2018/06/10',1);
+insert into Products values ("P11", 50, '2018/06/10',"- CPU: Intel Core i5, Dual - Core, 1.8GHz
 - RAM: 8GB, DDR3L, 1600 MHz, (On board),
 - Ổ cứng: SSD 256GB
 - Card màn hình: Intel HD Graphics 6000, Share
@@ -254,16 +255,16 @@ insert into Products values ("P11", 50, '2018/06/10',"Macbook Air",0, "./img/app
 - Kết nối: 2x USB 3.0, Thunderbolt ,MagSafe 2
 - Kích thước, trọng lượng: 314 x 219 x 18 mm, 1.06Kg
 - Hệ điều hành: Mac OS X
-",190000000,1,2,'2018/06/10');
-insert into Products values ("P12", 50, '2018/06/10',"Laptop Asus",0, "./img/asus-s510ua.jpg","- CPU: Intel Core i5 7300HQ (2.5GHz Up to 3.5GHz)
+",0, "/img/apple-macbook-air.jpg","Macbook Air",19000000,1,2,'2018/06/10',5);
+insert into Products values ("P12", 50, '2018/06/10',"- CPU: Intel Core i5 7300HQ (2.5GHz Up to 3.5GHz)
 - RAM: 1x8GB, DDR4, 2400MHz, 2 Slots
 - Ổ cứng: HDD 1TB 5400rpm SSH8G, khe mở rộng: M.2.2280
 - Card màn hình: NVIDIA GeForce GTX 1050 // Intel HD Graphics 630, 4GB
 - Màn hình: 15.6 INCH, 1920x1080, Webcam HD, Led_KB
 - Kết nối: 2x USB 3.0, USB 2.0, HDMI, BT 4.1
 - Hệ điều hành: Windows 10 Home SL 64bits 
-",16000000,1,1,'2018/06/10');
-insert into Products values ("P14", 30, '2018/06/10',"Laptop MSI",0, "./img/laptop-msi-gp63.jpg","Mô tả
+",0, "/img/asus-s510ua.jpg","Laptop Asus",16000000,1,1,'2018/06/10',5);
+insert into Products values ("P14", 30, '2018/06/10',"Mô tả
 - CPU: Intel Core i7-8750H (2.20 Ghz up to 4.10 Ghz, 9M Cache)
 - RAM: 2x8GB DDR4 2400 MHz (2 slots - max 32GB)
 - VGA: NVIDIA GeForce GTX 1050 Ti 4GB + Intel UHD Graphics 630
@@ -273,8 +274,8 @@ insert into Products values ("P14", 30, '2018/06/10',"Laptop MSI",0, "./img/lapt
 - Cổng giao tiếp: HDMI, miniDisplayPort, USB Type-C, 3x USB 3.0
 - Wifi AC, LAN 1Gbit, Bluetooth 5.0, Webcam HD, Card reader, Led Keyboard
 - Khối lượng: 2.2 Kg, pin 6 cell
-- Hệ điều hành: Win 10 Home",34000000,1,1,'2018/06/10');
-insert into Products values ("P15", 30, '2018/06/10',"Laptop DELL",0, "./img/dell_xps13_silver.jpg","Mô tả
+- Hệ điều hành: Win 10 Home",0, "/img/laptop-msi-gp63.jpg","Laptop MSI",34000000,1,1,'2018/06/10',4);
+insert into Products values ("P15", 30, '2018/06/10',"Mô tả
 - CPU: Intel Core i7-8550U (1.80 Ghz up to 4.0 Ghz, 8M Cache)
 - RAM: 8GB DDR4 2400 MHz (2 slots)
 - VGA: AMD 530 4GB + Intel UHD Graphics 620
@@ -285,8 +286,8 @@ insert into Products values ("P15", 30, '2018/06/10',"Laptop DELL",0, "./img/del
 - Wifi AC, LAN 100Mbit, Bluetooth 4.1
 - DVD-RW, Card reader, Led Keyboard
 - Khối lượng: 2.12 Kg, pin 3 cell
-- Hệ điều hành: Win 10 Home",39000000,1,0,'2018/06/10');
-insert into Products values ("P16", 30, '2018/06/10',"Laptop Lenovo",0, "./img/laptop_lenovo_ideapad.jpg","Mô tả
+- Hệ điều hành: Win 10 Home",0, "/img/dell_xps13_silver.jpg","Laptop DELL",39000000,1,0,'2018/06/10',4);
+insert into Products values ("P16", 30, '2018/06/10',"Mô tả
 - CPU: Intel Core i7-8550U (1.80 Ghz up to 4.0 Ghz, 8M Cache)
 - RAM: 8GB DDR4 2400 MHz (2 slots)
 - VGA: AMD 530 4GB + Intel UHD Graphics 620
@@ -297,8 +298,8 @@ insert into Products values ("P16", 30, '2018/06/10',"Laptop Lenovo",0, "./img/l
 - Wifi AC, LAN 100Mbit, Bluetooth 4.1
 - DVD-RW, Card reader, Led Keyboard
 - Khối lượng: 2.12 Kg, pin 3 cell
-- Hệ điều hành: Win 10 Home",15000000,1,1,'2018/06/10');
-insert into Products values ("P17", 30, '2018/06/10',"Laptop Acer",0, "./img/laptop_acer_nitro.jpg","Mô tả
+- Hệ điều hành: Win 10 Home",0, "/img/laptop_lenovo_ideapad.jpg","Laptop Lenovo",15000000,1,1,'2018/06/10',3);
+insert into Products values ("P17", 30, '2018/06/10',"Mô tả
 - CPU: Intel Core i7-8550U (1.80 Ghz up to 4.0 Ghz, 8M Cache)
 - RAM: 8GB DDR4 2400 MHz (2 slots)
 - VGA: AMD 530 4GB + Intel UHD Graphics 620
@@ -309,27 +310,27 @@ insert into Products values ("P17", 30, '2018/06/10',"Laptop Acer",0, "./img/lap
 - Wifi AC, LAN 100Mbit, Bluetooth 4.1
 - DVD-RW, Card reader, Led Keyboard
 - Khối lượng: 2.12 Kg, pin 3 cell
-- Hệ điều hành: Win 10 Home",23000000,1,0,'2018/06/10');
-insert into Products values ("P18", 30, '2018/06/10',"Laptop ACER",0, "./img/hp_probook_430.png","Mô tả
+- Hệ điều hành: Win 10 Home",0, "/img/laptop_acer_nitro.jpg","Laptop Acer",23000000,1,0,'2018/06/10',2);
+insert into Products values ("P18", 30, '2018/06/10',"Mô tả
 - CPU: Intel Core i7-7500U
 - Ram: 4GB
 - Ổ cứng: SSD 256GB
 - Màn hình: 13.3 HD
 - Trọng lượng: 1.5 Kg
-- Hệ điều hành có sẵn: Free Dos",17000000,1,0,'2018/06/10');
-insert into Products values ("P19", 30, '2018/06/10',"Laptop LG",0, "./img/LG_13ZD.jpg","Mô tả
+- Hệ điều hành có sẵn: Free Dos",0, "/img/hp_probook_430.png","Laptop ACER",17000000,1,0,'2018/06/10',3);
+insert into Products values ("P19", 30, '2018/06/10',"Mô tả
 - CPU: Core i5 7200U
 - Ram: 8GB DDR4
 - Ổ cứng: SSD 256GB
 - Màn hình: FHD 1920 x 1080 px
 - Trọng lượng: 0.94kg
-- Hệ điều hành có sẵn: Free Dos",17000000,1,1,'2018/06/10');
-insert into Products values ("P20", 30, '2018/06/10',"Loa Bluetooth",0, "./img/loa-bluetooth-cloud-fox.png","Công suất loa lớn cho âm thanh phát ra to, rõ và trầm ấm.
+- Hệ điều hành có sẵn: Free Dos",0, "/img/LG_13ZD.jpg","Laptop LG",17000000,1,1,'2018/06/10',4);
+insert into Products values ("P20", 30, '2018/06/10',"Công suất loa lớn cho âm thanh phát ra to, rõ và trầm ấm.
 Các nút ấn dạng cảm ứng khá nhạy với cú chạm nhẹ.
 Cách tăng giảm âm lượng độc đáo, tạo sự thích thú.
 Tự động ngủ sau 15 phút không kết nối hoặc phát nhạc.
 Có thể nhận và nghe gọi cuộc gọi trực tiếp trên loa.
 Công nghệ bluetooth 4.0 giúp kết nối tới hơn 10 m.
 Pin 2.200 mAh, chơi nhạc 12 giờ với âm lượng 75%.
-Chú ý: Dùng Adapter 5V-1A/2A để sạc cho thiết bị, Adapter trên 5V có thể gây ra hư hỏng cho thiết bị.",220000,4,1,'2018/06/10');
+Chú ý: Dùng Adapter 5V-1A/2A để sạc cho thiết bị, Adapter trên 5V có thể gây ra hư hỏng cho thiết bị.",0, "/img/loa-bluetooth-cloud-fox.png","Loa Bluetooth",220000,4,1,'2018/06/10',3);
 
